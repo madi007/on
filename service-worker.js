0be2eef4919a1ebf13bdfc6b21361b403aba87fb
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-16 06:05:45 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,21 @@
-const CACHE_NAME = 'pwa-cache-1';
-const urlsToCache = [
-    '/',
-    'index.html',
-    'style.css',
-    'script.js',
-    'app.js',
-    'manifest.json',
-    'index.html' // Оффлайн-страница
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./script.js",
+        "./style.css",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ])
+    )
+  );
 });
 
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        console.log('Deleting old cache:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => caches.match('index.html'));
-        })
-    );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
