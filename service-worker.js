837bbc9c8582f91fc9a90978da83da7918c858ba
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-16 05:44:30 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,43 @@
-self.addEventListener('install', function(e) {
-    console.log('Service Worker: Installed');
-  });
-  
-  self.addEventListener('fetch', function(e) {
-    // ты можешь тут кэшировать запросы
-  });
-  
+const CACHE_NAME = 'pwa-cache-1';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'style.css',
+    'script.js',
+    'app.js',
+    'manifest.json',
+    'index.html' // Оффлайн-страница
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => caches.match('index.html'));
+        })
+    );
+});
